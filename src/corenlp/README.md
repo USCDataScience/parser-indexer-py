c73e847b122ed5bd4db89b3887a2544eafbf7221
Thamme Gowda <thammegowda@apache.org>
2016-08-09 15:29:33 -0700

    added brat to ner format converter

---
+++ src/corenlp/README.md
@@ -0,0 +1,88 @@
+# Training a custom Named Entity Recogniser using Stanford CoreNLP
+
+This document contains instructions to train Core NLP's CRFClassifier for NER.
+
+## References:
+1. Create custom NER model for CoreNLP http://nlp.stanford.edu/software/crf-faq.shtml#a
+2. Brat : http://brat.nlplab.org/
+3. Py CoreNLP https://github.com/smilli/py-corenlp
+
+## Step 1. Download CoreNLP
+Visit http://stanfordnlp.github.io/CoreNLP/download.html,
+download the zip.
+Extract the zip.
+Note: this runs on Java 8
+
+## Step 2: Install Python dependencies
+
+Follow instructions in https://github.com/smilli/py-corenlp
+
+    pip install pycorenlp
+
+## Step 3: Start Core NLP server
+Goto CoreNLP extracted directory and runs
+
+    java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
+
+## Step 4: Prepare input text and annotations
+List all the `.txt` files and `.ann` files to a path list file
+
+    $ ls $PWD/*.txt > 1.list
+    $ ls $PWD/*.ann > 2.list
+    $ paste  -d "," 1.list  2.list  > input.list
+
+## Step 5: Separate the training and testing records
+
+Split the `input.list` for training and testing data. Lets call them as `train.list` and `test.list`
+
+## Step 6: Convert brat to corenlp NER format
+
+
+    python3 brat2ner.py --in train.list --out train.tsv
+    python3 brat2ner.py --in test.list --out test.tsv
+
+## Step 6: Train and create a NER Model
+
+Create a properties file lpsc.prop
+
+```
+#location of the training file
+trainFile = train.tsv
+#location where you would like to save (serialize to) your
+#classifier; adding .gz at the end automatically gzips the file,
+#making it faster and smaller
+serializeTo = ner-model.ser.gz
+
+#structure of your training file; this tells the classifier
+#that the word is in column 0 and the correct answer is in
+#column 1
+map = word=0,answer=1
+
+#these are the features we'd like to train with
+#some are discussed below, the rest can be
+#understood by looking at NERFeatureFactory
+useClassFeature=true
+useWord=true
+useNGrams=true
+#no ngrams will be included that do not contain either the
+#beginning or end of the word
+noMidNGrams=true
+useDisjunctive=true
+maxNGramLeng=6
+usePrev=true
+useNext=true
+useSequences=true
+usePrevSequences=true
+maxLeft=1
+#the next 4 deal with word shape features
+useTypeSeqs=true
+useTypeSeqs2=true
+useTypeySequences=true
+wordShape=chris2useLC
+```
+
+    java -cp .:../stanford-corenlp-full-2015-12-09/* \
+     edu.stanford.nlp.ie.crf.CRFClassifier  -prop lpsc.prop
+
+## Step 7: Test
+    java -cp .:../stanford-corenlp-full-2015-12-09/* edu.stanford.nlp.ie.crf.CRFClassifier  -loadClassifier ner-model.ser.gz -testFile test.tsv
---
+++ src/corenlp/brat2ner.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+from pycorenlp import StanfordCoreNLP
+import os
+from argparse import ArgumentParser
+
+class BratToNerConverter(object):
+    def __init__(self, corenlp_url='http://localhost:9000'):
+        '''
+        Create Converter for converting brat annotations to Core NLP NER CRF
+        classifier training data.
+
+        To start the server checkout: http://stanfordnlp.github.io/CoreNLP/corenlp-server.html#getting-started
+        '''
+        self.corenlp = StanfordCoreNLP(corenlp_url)
+
+    def convert(self, text_file, ann_file):
+        text, tree = self.parse(text_file, ann_file)
+        props = { 'annotators': 'tokenize,ssplit', 'outputFormat': 'json'}
+        output = self.corenlp.annotate(text, properties=props)
+        records = []
+        for sentence in output['sentences']:
+            for tok in sentence['tokens']:
+                begin, end = tok['characterOffsetBegin'], tok['characterOffsetEnd']
+                label = 'O'
+                if begin in tree:
+                    node = tree[begin]
+                    if end in node:
+                        labels = node[end]
+                        assert len(labels) == 1 # havent seen the overlap, but interested to see
+                        label = labels[0]
+                    else:
+                        print("ERROR: Multi token words are not handled")
+                yield "%s\t%s" % (tok['word'], label)
+            yield "" # end of sentence
+
+    def parse(self, txt_file, ann_file):
+        with open(txt_file) as text_file, open(ann_file) as ann_file:
+            texts = text_file.read()
+            anns = map(lambda x: x.strip().split('\t'), ann_file)
+            anns = filter(lambda x: len(x) > 2, anns)
+            # FIXME: ignoring the annotatiosn which are complex
+
+            anns = filter(lambda x: ';' not in x[1], anns)
+            # FIXME: some annotations' spread have been split into many, separated by ; ignoring them
+
+            def __parse_ann(ann):
+                spec = ann[1].split()
+                name = spec[0]
+                markers = list(map(lambda x: int(x), spec[1:]))
+                t = ' '.join([texts[begin:end] for begin,end in zip(markers[::2], markers[1::2])])
+                if not t == ann[2]:
+                    print("Error: Annotation mis-match, file=%s, ann=%s" % (txt_file, str(ann)))
+                    return None
+                return (name, markers, t)
+            anns = map(__parse_ann, anns) # format
+            anns = filter(lambda x: x, anns) # skip None
+
+            # building a tree index for easy accessing
+            tree = {}
+            for entity_type, pos, name in anns:
+                begin, end = pos[0], pos[1]
+                if begin not in tree:
+                    tree[begin] = {}
+                node = tree[begin]
+                if end not in node:
+                    node[end] = []
+                node[end].append(entity_type)
+            return texts, tree
+
+    def convert_all(self, input_paths, output):
+        with open(input_paths) as paths, open(output, 'w') as out:
+            for p in map(lambda x: x.strip(), paths):
+                d = p.split(',')
+                print(d)
+                for line in self.convert(d[0], d[1]):
+                    out.write(line)
+                    out.write("\n")
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser(description="Brat to CoreNLP NER annotation converter")
+    parser.add_argument("--in", help="""Input file, each line contains comma separated *.txt,*.ann\n
+    To create the input file, follow these instructuions:
+    $ ls $PWD/*.txt > 1.list
+    $ ls $PWD/*.ann > 2.list
+    $ paste  -d "," 1.list  2.list  > input.list""", required=True)
+    parser.add_argument("--out", help="""Output file""", required=True)
+    args = vars(parser.parse_args())
+    BratToNerConverter().convert_all(args['in'], args['out'])
