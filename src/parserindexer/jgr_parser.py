8ff4d31f5c5085b4ceebc4d8d4366f88cb9cd362
Steven Lu <you.lu@jpl.nasa.gov>
2021-04-29 10:59:30 -0700

    (1) add paper_parser; (2) rename journal_parser to jgr_parser; (3) update lpsc and jgr parsers; #15

--- src/parserindexer/jgr_parser.py
+++ src/parserindexer/jgr_parser.py
@@ -1,65 +1,31 @@
 from __future__ import print_function
 
-import re
 import sys
 import json
-from parser import Parser
 from ioutils import read_lines
+from paper_parser import PaperParser
 from tika_parser import TikaParser
 from jsre_parser import JsreParser
-from brat_ann_indexer import extract_references
 
 
-class JournalParser(Parser):
-    """ The JournalParser removes special text/format for journal articles """
+class JgrParser(PaperParser):
+    """ The JgrParser removes special text/format for the Journal of
+    Geophysical Research (JGR)
+    """
     PDF_TYPE = "application/pdf"
 
     def __init__(self):
-        super(JournalParser, self).__init__('journal_parser')
+        super(JgrParser, self).__init__('journal_parser')
 
     def parse(self, text, metadata):
-        if type(metadata['Content-Type']) == list:
-            assert JournalParser.PDF_TYPE in metadata['Content-Type']
-        else:
-            assert metadata['Content-Type'] == JournalParser.PDF_TYPE
+        paper_dict = super(JgrParser, self).parse(text, metadata)
+        cleaned_text = paper_dict['cleaned_content']
 
-        assert type(text) == str or type(text) == unicode
-
-        # 1. Translate some UTF-8 punctuation to ASCII
-        punc = {
-            # single quote
-            0x2018: 0x27, 0x2019: 0x27,
-            # double quote
-            0x201C: 0x22, 0x201D: 0x22,
-            # hyphens
-            0x2010: 0x2d, 0x2011: 0x2d, 0x2012: 0x2d, 0x2013: 0x2d,
-            # comma
-            0xFF0C: 0x2c,
-            # space
-            0x00A0: 0x20,
-            # bullets
-            0x2219: 0x2e, 0x2022: 0x2e,
-        }
-        text = text.translate(punc)
-
-        # 2. Replace newlines that separate words with a space (unless hyphen)
-        text = re.sub(r'([^\s-])[\r|\n]+([^\s])', '\\1 \\2', text)
-
-        # 3. Remove hyphenation at the end of lines
-        # (this is sometimes bad, as with "Fe-\nrich")
-        text = text.replace('-\n', '\n')
-
-        # 4. Remove all newlines
-        text = re.sub(r'[\r|\n]+', '', text)
-
-        # 5. Move references to their own field (references)
-        refs = extract_references(text)
-        for ref_id in refs:  # preserve length; insert whitespace
-            text = text.replace(refs[ref_id], ' ' * len(refs[ref_id]))
+        # TODO: add details
 
         return {
-            'references': refs.values(),
-            'cleaned_content': text
+            'references': paper_dict['references'],
+            'cleaned_content': cleaned_text
         }
 
 
@@ -70,7 +36,7 @@ def process(in_file, in_list, out_file, tika_server_url, corenlp_server_url,
         sys.exit(1)
 
     tika_parser = TikaParser(tika_server_url)
-    journal_parser = JournalParser()
+    jgr_parser = JgrParser()
     jsre_parser = JsreParser(corenlp_server_url, ner_model, jsre_root,
                              jsre_model, jsre_tmp_dir)
 
@@ -82,8 +48,8 @@ def process(in_file, in_list, out_file, tika_server_url, corenlp_server_url,
     out_f = open(out_file, 'wb', 1)
     for f in files:
         tika_dict = tika_parser.parser(f)
-        journal_dict = journal_parser.parse(tika_dict['content'],
-                                            tika_dict['metadata'])
+        journal_dict = jgr_parser.parse(tika_dict['content'],
+                                        tika_dict['metadata'])
         jsre_dict = jsre_parser.parse(journal_dict['cleaned_content'])
 
         tika_dict['content_ann_s'] = journal_dict['cleaned_content']
--- src/parserindexer/lpsc_parser.py
+++ src/parserindexer/lpsc_parser.py
@@ -3,85 +3,42 @@ from __future__ import print_function
 import re
 import sys
 import json
-from parser import Parser
 from ioutils import read_lines
+from paper_parser import PaperParser
 from tika_parser import TikaParser
 from jsre_parser import JsreParser
-from brat_ann_indexer import extract_references
 
 
-class LpscParser(Parser):
+class LpscParser(PaperParser):
     """ The LpscParser removes special text content/format (e.g., LPSC
     conference header) from the two-page abstracts published at Lunar and
     Planetary Science Conference
     """
-    JOURNAL_PARSER = 'org.apache.tika.parser.journal.JournalParser'
     PDF_TYPE = "application/pdf"
 
-    def __init__(self):
-        super(LpscParser, self).__init__('lpsc_parser')
+    def __init__(self, parser_name='lpsc_parser'):
+        super(LpscParser, self).__init__(parser_name)
 
     def parse(self, text, metadata):
-        if type(metadata['Content-Type']) == list:
-            assert LpscParser.PDF_TYPE in metadata['Content-Type']
-        else:
-            assert metadata['Content-Type'] == LpscParser.PDF_TYPE
+        paper_dict = super(LpscParser, self).parse(text, metadata)
+        cleaned_text = paper_dict['cleaned_content']
 
-        # Improve parsing and save in parsed['content_ann_s']
-        assert type(text) == str or type(text) == unicode
-
-        # New parsing (after extract_text_utf8.py)
-        # 0. Translate some UTF-8 punctuation to ASCII
-        punc = {
-            # single quote
-            0x2018: 0x27, 0x2019: 0x27,
-            # double quote
-            0x201C: 0x22, 0x201D: 0x22,
-            # hyphens
-            0x2010: 0x2d, 0x2011: 0x2d, 0x2012: 0x2d, 0x2013: 0x2d,
-            # comma
-            0xFF0C: 0x2c,
-            # space
-            0x00A0: 0x20,
-            # bullets
-            0x2219: 0x2e, 0x2022: 0x2e,
-        }
-        text = text.translate(punc)
-
-        # 1. Replace newlines that separate words with a space (unless hyphen)
-        text = re.sub(r'([^\s-])[\r|\n]+([^\s])', '\\1 \\2', text)
-
-        # 2. Remove hyphenation at the end of lines
-        # (this is sometimes bad, as with "Fe-\nrich")
-        text = text.replace('-\n', '\n')
-
-        # 3. Remove all newlines
-        text = re.sub(r'[\r|\n]+', '', text)
-
-        # 4. Remove xxxx.PDF
-        text = re.sub(r'([0-9][0-9][0-9][0-9].PDF)', '', text,
-                      flags=re.IGNORECASE)
+        # Remove xxxx.PDF
+        cleaned_text = re.sub(r'([0-9][0-9][0-9][0-9].PDF)', '', cleaned_text,
+                              flags=re.IGNORECASE)
         # And "xx(th|st) Lunar and Planetary Science Conference ((19|20)xx)"
-        text = re.sub(r'([0-9][0-9].. Lunar and Planetary Science Conference '
-                      r'\((19|20)[0-9][0-9]\)) ?', '', text,
-                      flags=re.IGNORECASE)
+        cleaned_text = re.sub(r'([0-9][0-9].. Lunar and Planetary Science '
+                              r'Conference \((19|20)[0-9][0-9]\)) ?', '',
+                              cleaned_text, flags=re.IGNORECASE)
         # And "Lunar and Planetary Science XXXIII (2002)"
         # with Roman numeral and optional year
-        text = re.sub(r'(Lunar and Planetary Science '
-                      r'[CDILVXM]+( \((19|20)[0-9][0-9]\))?) ?', '', text,
-                      flags=re.IGNORECASE)
-
-        # 5. Remove mailto: links
-        text = re.sub(r'mailto:[^\s]+', '', text)
-
-        # 6. Move references to their own field (references)
-        refs = extract_references(text)
-        for ref_id in refs:  # preserve length; insert whitespace
-            text = text.replace(refs[ref_id], ' ' * len(refs[ref_id]))
+        cleaned_text = re.sub(r'(Lunar and Planetary Science '
+                              r'[CDILVXM]+( \((19|20)[0-9][0-9]\))?) ?', '',
+                              cleaned_text, flags=re.IGNORECASE)
 
         return {
-            'references': refs.values(),
-            'cleaned_content': text
+            'references': paper_dict['references'],
+            'cleaned_content': cleaned_text
         }
 
 
